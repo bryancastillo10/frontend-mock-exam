fbd2306224ec4a4a2e08feafb35ba622e0f2dbed
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2024-10-22 05:16:47 +0800

    style: initialize search result

--- src/components/main/SearchResults.tsx
+++ src/components/main/SearchResults.tsx
@@ -1,6 +1,9 @@
 import { gameDataProps } from "../../api/useGetGames";
 import Cards from "../reusables/Cards";
 
+import { IoChevronBackOutline } from "react-icons/io5";
+import { IoChevronForward } from "react-icons/io5";
+
 interface SearchResultsProps {
   results: gameDataProps[];
   toggleFavorite: (id: number) => void;
@@ -8,17 +11,26 @@ interface SearchResultsProps {
 
 const SearchResults = ({ results, toggleFavorite }: SearchResultsProps) => {
   return (
-    <section className="grid grid-cols-3 gap-y-3 px-8 xl:px-2 justify-center">
-      {results.map((res) => (
-        <Cards
-          key={res.id}
-          gameId={res.id}
-          hasFavorited={res.hasFavorited}
-          image={res.img}
-          toggleFavorite={toggleFavorite}
-        />
-      ))}
-    </section>
+    <>
+      <section className="grid grid-cols-3 gap-y-3 px-8 xl:px-2 justify-center">
+        {results.map((res) => (
+          <Cards
+            key={res.id}
+            gameId={res.id}
+            hasFavorited={res.hasFavorited}
+            image={res.img}
+            toggleFavorite={toggleFavorite}
+          />
+        ))}
+      </section>
+      <div className="mt-2 px-4 flex justify-between">
+        <p>Proveedores de juego</p>
+        <div className="flex gap-8 items-center mr-4">
+          <IoChevronBackOutline size="24" />
+          <IoChevronForward size="24" />
+        </div>
+      </div>
+    </>
   );
 };
 
