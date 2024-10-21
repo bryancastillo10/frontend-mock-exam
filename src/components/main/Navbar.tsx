6a664195dc5e5bd926b5710b365f6edc0b3974fd
Bryan Castillo <bryan.angelocastillo.10@gmail.com>
2024-10-21 19:23:54 +0800

    style: initialize navbar

--- src/components/main/Navbar.tsx
+++ src/components/main/Navbar.tsx
@@ -1,4 +1,19 @@
+import { BiMenuAltLeft } from "react-icons/bi";
+import { IoWallet } from "react-icons/io5";
+import NavLogo from "../../assets/F1M5_Logo.png";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="px-3 py-4 border shadow-md flex justify-between">
+      <div className="flex gap-3 items-center">
+        <BiMenuAltLeft size="24" className="text-primary" />
+        <img src={NavLogo} className="h-5 max-w-sm" />
+      </div>
+      <div className="flex gap-2 items-center">
+        <IoWallet size="24" className="text-secondary" />
+        <h1 className="font-semibold text-primary">$ 1990.6</h1>
+      </div>
+    </nav>
+  );
 };
 export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ export default {
       },
       fontWeight: {
         regular: 400,
+        semibold: 600,
         bold: 700,
       },
       fontSize: {
